c120088ff1845bf3aafbc587cce907d8980d2b81
Eve.Grna <pop.cake@mail.ru>
2023-11-24 14:04:43 +0300

    Skelet

---
+++ funcs.cpp
@@ -0,0 +1,11 @@
+//funcs.cpp
+#include "funcs.h"
+#include <cmath>
+
+double MySum(double a, double b) 
+
+double MySub(double a, double b)
+
+double MyMul(double a, double b) 
+
+double MyDiv(double a, double b) 
---
+++ program.cpp
@@ -0,0 +1,45 @@
+//program.cpp
+#include <iostream>
+#include "funcs.h" 
+using namespace std;
+int main()
+{
+	double a, b;
+	while (count!=0)
+	{
+		cout << "Vvedite pervoe chislo: "; 
+		cin >> a; 
+		cout << "Vvedite vtoroe chislo: "; 
+		cin >> b; 
+		cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie; 0-exit: "; 
+		cin >> count; 
+		switch (count) // 
+		{ 
+			case 1: // count = 1 
+			{ 
+				cout << " V razrabotke " << endl;
+				break;
+			} 
+			case 2: // count = 2 
+			{
+				cout << " V razrabotke " << endl;
+				break;
+			}
+			case 3: // count = 3 
+				{ 
+				cout << " V razrabotke " << endl;
+				break;
+			}
+			case 4: // count = 4
+			{
+				cout << " V razrabotke " << endl;
+				break;
+			} 
+			default: 
+				cout << "Nepravilni vvod" << endl;
+		} 
+	}
+	system("pause"); 
+	return 0; 
+}
+	
