19dd094376e1a903aaeeab29d9e82baff2c1e7d2
Eve.Grna <pop.cake@mail.ru>
2023-11-30 18:01:30 +0300

    Structure changed

--- program.cpp
+++ program.cpp
@@ -1,9 +1,12 @@
 //program.cpp
 #include <iostream>
 #include "funcs.h" 
+
 using namespace std;
+
 int main()
 {
+	int count = 5;
 	double a, b;
 	while (count!=0)
 	{
@@ -13,29 +16,28 @@ int main()
 		cin >> b; 
 		cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie; 0-exit: "; 
 		cin >> count; 
-		switch (count) // 
+		switch (count) 
 		{ 
-			case 1: // count = 1 
+			case 1:  
 			{ 
 				cout << a << " + " << b << " = " << MySum(a, b) << endl;
 				break;
 			} 
-			case 2: // count = 2 
+			case 2: 
 			{
 				cout << a << " - " << b << " = " << MySub(a, b) << endl;
 				break;
 			}
-                        case 3:  count = 3
-                        {
-                                 cout << a << " * " << b << " = " << MyMul(a, b) << endl;
-                                 break;
-                        }
-                        case 4:  count = 4
-                        {
-                                  cout << a << " / " << b << " = " << MyDiv(a, b) << endl;
-                                  break;
-                        }
-
+			case 3:  count = 3
+					 {
+						 cout << a << " * " << b << " = " << MyMul(a, b) << endl;
+						 break;
+					 }
+			case 4:  count = 4
+					 {
+						 cout << a << " / " << b << " = " << MyDiv(a, b) << endl;
+						 break;
+					 }
 			default: 
 				cout << "Nepravilni vvod" << endl;
 		} 
